4b300252bb777d87152cc39ccbfdf6e4b21db3ec
aesteve <aesteve@evenium.com>
2015-11-05 19:11:38 +0100

    @auth => wip

--- src/main/java/io/projuice/controllers/api/UserApiController.java
+++ src/main/java/io/projuice/controllers/api/UserApiController.java
@@ -7,8 +7,10 @@ import javax.persistence.criteria.CriteriaQuery;
 
 import com.github.aesteve.nubes.hibernate.annotations.RetrieveByQuery;
 import com.github.aesteve.vertx.nubes.annotations.Controller;
+import com.github.aesteve.vertx.nubes.annotations.auth.Auth;
 import com.github.aesteve.vertx.nubes.annotations.mixins.ContentType;
 import com.github.aesteve.vertx.nubes.annotations.routing.http.GET;
+import com.github.aesteve.vertx.nubes.auth.AuthMethod;
 import com.github.aesteve.vertx.nubes.context.PaginationContext;
 
 @Controller("/api/1/users")
@@ -17,6 +19,7 @@ public class UserApiController {
 
 	@GET
 	@RetrieveByQuery
+	@Auth(method = AuthMethod.REDIRECT, authority = "superuser", redirectURL = "/login")
 	public CriteriaQuery<ProjuiceUser> getAllUsers(CriteriaBuilder builder, PaginationContext pageContext) {
 		CriteriaQuery<ProjuiceUser> crit = builder.createQuery(ProjuiceUser.class);
 		crit.from(ProjuiceUser.class);
