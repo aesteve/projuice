1a541d2e69e6b7eb374fa8eebd83ce8a78f1c204
aesteve <aesteve@evenium.com>
2015-11-06 18:26:50 +0100

    logincontroller

---
+++ src/main/java/io/projuice/controllers/api/LoginApiController.java
@@ -0,0 +1,70 @@
+package io.projuice.controllers.api;
+
+import static com.github.aesteve.vertx.nubes.auth.AuthMethod.API_TOKEN;
+import static io.projuice.auth.ProjuiceAuthProvider.LOGGED_IN;
+import io.projuice.Server;
+import io.projuice.auth.AuthenticationException;
+import io.projuice.auth.ProjuiceAuthProvider;
+import io.projuice.model.ProjuiceUser;
+import io.projuice.services.TokenService;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.web.RoutingContext;
+
+import com.github.aesteve.vertx.nubes.annotations.Controller;
+import com.github.aesteve.vertx.nubes.annotations.auth.Auth;
+import com.github.aesteve.vertx.nubes.annotations.auth.User;
+import com.github.aesteve.vertx.nubes.annotations.mixins.ContentType;
+import com.github.aesteve.vertx.nubes.annotations.params.RequestBody;
+import com.github.aesteve.vertx.nubes.annotations.routing.http.POST;
+import com.github.aesteve.vertx.nubes.annotations.services.Service;
+import com.github.aesteve.vertx.nubes.marshallers.Payload;
+
+@Controller("/api/1")
+@ContentType("application/json")
+public class LoginApiController {
+
+	@Service(Server.AUTH_SERVICE)
+	ProjuiceAuthProvider authProvider;
+
+	@Service(Server.TOKEN_SERVICE)
+	TokenService tokenService;
+
+	@POST("/login")
+	public void login(RoutingContext context, @RequestBody JsonObject credentials, Payload<JsonObject> loginInfo) {
+		authProvider.authenticateByUsername(credentials, res -> {
+			if (res.failed()) {
+				Throwable cause = res.cause();
+				if (cause instanceof AuthenticationException) {
+					context.fail(401);
+				} else {
+					context.fail(cause);
+				}
+				return;
+			}
+			ProjuiceUser user = res.result();
+			if (user == null) {
+				context.fail(401);
+				return;
+			}
+			context.setUser(user);
+			tokenService.getTokenFor(user, tokenRes -> {
+				if (tokenRes.failed()) {
+					context.fail(tokenRes.cause());
+					return;
+				}
+				JsonObject userInfo = new JsonObject();
+				userInfo.put("username", user.getUsername());
+				userInfo.put("access_token", tokenRes.result());
+				loginInfo.set(userInfo);
+				context.next();
+			});
+		});
+	}
+
+	@POST("/logout")
+	@Auth(method = API_TOKEN, authority = LOGGED_IN)
+	public void logout(@User ProjuiceUser current, Payload<Void> payload) {
+		authProvider.clearFor(current);
+		payload.set(null);
+	}
+}
--- src/test/java/integration/auth/LoginSpec.java
+++ src/test/java/integration/auth/LoginSpec.java
@@ -16,7 +16,7 @@ public class LoginSpec extends ProjuiceTestBase {
 		JsonObject loginData = new JsonObject();
 		loginData.put("username", username);
 		loginData.put("password", "secret");
-		postJSON(null, "/api/1/users/login", loginData, response -> {
+		postJSON(null, "/api/1/login", loginData, response -> {
 			context.assertEquals(200, response.statusCode());
 			response.bodyHandler(buff -> {
 				JsonObject loginInfo = new JsonObject(buff.toString("UTF-8"));
@@ -41,13 +41,13 @@ public class LoginSpec extends ProjuiceTestBase {
 		JsonObject loginData = new JsonObject();
 		loginData.put("username", username);
 		loginData.put("password", "secret");
-		postJSON(null, "/api/1/users/login", loginData, response -> {
+		postJSON(null, "/api/1/login", loginData, response -> {
 			context.assertEquals(200, response.statusCode());
 			response.bodyHandler(buff -> {
 				JsonObject loginInfo = new JsonObject(buff.toString("UTF-8"));
 				String accessToken = loginInfo.getString("access_token");
 				context.assertNotNull(accessToken);
-				postJSON(accessToken, "/api/1/users/logout", new JsonObject(), logoutResponse -> {
+				postJSON(accessToken, "/api/1/logout", new JsonObject(), logoutResponse -> {
 					context.assertEquals(204, logoutResponse.statusCode());
 					getJSON(accessToken, "/api/1/users/me", myResponse -> {
 						context.assertEquals(401, myResponse.statusCode());
