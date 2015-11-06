2640a1cf5423af4cbf79da81856a0d44c4578d91
aesteve <aesteve@evenium.com>
2015-11-06 15:01:38 +0100

    add Provider as available service

--- src/main/java/io/projuice/Server.java
+++ src/main/java/io/projuice/Server.java
@@ -10,11 +10,18 @@ import com.github.aesteve.nubes.hibernate.HibernateNubesServer;
 import com.github.aesteve.nubes.hibernate.services.HibernateService;
 
 public class Server extends HibernateNubesServer {
+
+	public static final String TOKEN_SERVICE = "tokens";
+	public static final String AUTH_SERVICE = "auth-service";
+
 	@Override
 	public void init(Vertx vertx, Context context) {
 		super.init(vertx, context);
 		HibernateService hibernate = (HibernateService) nubes.getService(HibernateNubes.HIBERNATE_SERVICE_NAME);
-		System.out.println("hibernate ? " + hibernate);
-		nubes.setAuthProvider(new ProjuiceAuthProvider(hibernate, TokenService.create()));
+		TokenService tokens = TokenService.create();
+		ProjuiceAuthProvider provider = new ProjuiceAuthProvider(hibernate, tokens);
+		nubes.registerService(TOKEN_SERVICE, tokens);
+		nubes.registerService(AUTH_SERVICE, provider);
+		nubes.setAuthProvider(provider);
 	}
 }
--- src/main/java/io/projuice/auth/ProjuiceAuthProvider.java
+++ src/main/java/io/projuice/auth/ProjuiceAuthProvider.java
@@ -5,16 +5,21 @@ import io.projuice.services.TokenService;
 import io.vertx.core.AsyncResult;
 import io.vertx.core.Future;
 import io.vertx.core.Handler;
+import io.vertx.core.Vertx;
 import io.vertx.core.json.JsonObject;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
 import io.vertx.ext.auth.AuthProvider;
 import io.vertx.ext.auth.User;
 
-import com.github.aesteve.nubes.hibernate.queries.FindById;
+import com.github.aesteve.nubes.hibernate.queries.FindBy;
 import com.github.aesteve.nubes.hibernate.services.HibernateService;
+import com.github.aesteve.vertx.nubes.services.Service;
 
-public class ProjuiceAuthProvider implements AuthProvider {
+public class ProjuiceAuthProvider implements AuthProvider, Service {
+
+	public final static String SUPER_USER = "SUPER_USER";
+	public final static String LOGGED_IN = "LOGGED_IN";
 
 	protected final static Logger log = LoggerFactory.getLogger(ProjuiceAuthProvider.class);
 
@@ -27,19 +32,25 @@ public class ProjuiceAuthProvider implements AuthProvider {
 	}
 
 	@Override
+	public void init(Vertx vertx, JsonObject json) {}
+
+	@Override
+	public void start(Future<Void> future) {
+		future.complete();
+	}
+
+	@Override
+	public void stop(Future<Void> future) {
+		future.complete();
+	}
+
+	@Override
 	public void authenticate(JsonObject authInfo, Handler<AsyncResult<User>> resultHandler) {
-		String token = authInfo.getString("token");
+		String token = authInfo.getString("access_token");
 		if (token != null) {
 			authenticateByToken(token, resultHandler);
 			return;
 		}
-		String username = authInfo.getString("username");
-		String pwd = authInfo.getString("password");
-		if (username == null || pwd == null) {
-			resultHandler.handle(Future.failedFuture("No username or password specified"));
-			return;
-		}
-		authenticateByUsername(username, pwd, resultHandler);
 	}
 
 	public void clearFor(ProjuiceUser user) {
@@ -50,7 +61,7 @@ public class ProjuiceAuthProvider implements AuthProvider {
 	 * FIXME / TODO : describe business rules (who can see ?)
 	 */
 	public void isAuthorised(User user, String authority, Handler<AsyncResult<Boolean>> resultHandler) {
-		log.info("is Authorised called");
+		log.info("is Authorised called " + authority);
 		resultHandler.handle(Future.succeededFuture(Boolean.TRUE));
 	}
 
@@ -58,26 +69,49 @@ public class ProjuiceAuthProvider implements AuthProvider {
 		tokenService.getUserAssociatedWith(token, resultHandler);
 	}
 
-	private void authenticateByUsername(String username, String pwd, Handler<AsyncResult<User>> resultHandler) {
+	public void authenticateByUsername(JsonObject authInfo, Handler<AsyncResult<ProjuiceUser>> resultHandler) {
+		String username = authInfo.getString("username");
+		String pwd = authInfo.getString("password");
+		if (username == null || pwd == null) {
+			resultHandler.handle(Future.failedFuture(new AuthenticationException()));
+			return;
+		}
 		hibernate.withEntityManager((em, future) -> {
-			hibernate.findById(em, new FindById<>(ProjuiceUser.class, username), userResult -> {
+			hibernate.findBy(em, new FindBy<>(ProjuiceUser.class, "username", username), userResult -> {
+				System.out.println("FindUserByUsername : " + username);
+				System.out.println(userResult.failed());
 				if (userResult.failed()) {
 					future.fail(userResult.cause());
 					return;
 				}
 				ProjuiceUser user = userResult.result();
+				System.out.println(user);
 				if (user == null) {
 					future.fail(new AuthenticationException());
 					return;
 				}
-				if (pwd.equals(user.getPassword())) {
+				System.out.println(user.getPassword());
+				System.out.println(pwd.equals(user.getPassword()));
+				if (!pwd.equals(user.getPassword())) {
 					future.fail(new AuthenticationException());
 					return;
 				}
 				tokenService.createTokenFor(user, token -> {
+					System.out.println("create token for : ");
+					System.out.println(user);
 					future.complete(user);
 				});
 			});
-		}, resultHandler);
+		}, res -> {
+			if (res.failed()) {
+				System.out.println("with entitiy manager faield");
+				resultHandler.handle(Future.failedFuture(res.cause()));
+			} else {
+				System.out.println("with entitiy manager result");
+				System.out.println(res.result());
+				resultHandler.handle(Future.succeededFuture((ProjuiceUser) res.result()));
+			}
+		});
 	}
+
 }
